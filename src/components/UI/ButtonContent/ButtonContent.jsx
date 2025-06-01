df48a9c409b1030bfe911d76da83191ee53f3d87
l1ha4 <l1hach.main@gmail.com>
2025-06-01 15:06:06 +0300

    test-icon-4

--- src/components/UI/ButtonContent/ButtonContent.jsx
+++ src/components/UI/ButtonContent/ButtonContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import cl from './ButtonContent.module.css'
 
+
 function ButtonContent({ cN, children, ...props }) {
   return <button className={`${cl.button} ${cN}`} {...props}>{children}</button>
 }
