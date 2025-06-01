c97bfe5dbd38e74bbfb9808523d3dbbbac783374
l1ha4 <l1hach.main@gmail.com>
2025-06-01 15:26:07 +0300

    fix-icon-position-2

--- src/components/UI/SelectIcons/SelectItem.jsx
+++ src/components/UI/SelectIcons/SelectItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import cl from './SelectIcons.module.css'
 
-function SelectIcon({name, path, ...props}) {
+function SelectIcon({ name, path, ...props }) {
   return (
-    <div {...props} className={cl.item} >
-      <img src={path} alt={path} className={cl.icon}/>
+    <div {...props} className={cl.item}>
+      <img src={path} className={cl.icon} />
       <span className={cl.text}>{name}</span>
     </div>
   )
